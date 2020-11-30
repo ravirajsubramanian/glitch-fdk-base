1f198ecd8a78d8b8c34a288d679074f28331b287
Raviraj Subramanian <raviraj.subramanian@freshworks.com>
2020-11-30 15:53:08 +0530

    refactor: fixed node_modules path in some fdk files to work in Glitch

--- .fdk/fdk/package/lib/routes/app.js
+++ .fdk/fdk/package/lib/routes/app.js
@@ -10,7 +10,7 @@ const appUtil = require(`${global.FDK_PATH}/lib/utils/app-util`);
 const fileUtil = require(`${global.FDK_PATH}/lib/utils/file-util`);
 const nsUtil = fileUtil.nsresolver;
 
-const Router = require(`${global.FDK_PATH}/node_modules/express`).Router;
+const Router = require(`./../../../../../node_modules/express`).Router;
 const appsRouter = new Router();
 const LOCAL_URL = 'http://localhost:3000';
 let oauthStatus = 'notstarted';
